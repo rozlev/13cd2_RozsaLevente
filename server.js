6dca7706b570ad8b29eab2162aa1d329a5f5265f
rozlev <rozlev404@hengersor.hu>
2025-02-17 14:31:53 +0100

    select használata

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ app.get("/", (req, res) => {
     res.send("A szerver működik!");
 })
 
+
+app.get("/v",(req, res) => {
+    const sql = "SELECT * FROM `versenyzok`";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/v6",(req, res) => {
+    const sql = "SELECT * FROM `versenyzok` WHERE ID = 6;";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
 app.listen(3000, () => {
     console.log("A szerver a 3000 porton fut!");
 });
